31a784be860a0a3c44451b1ef944cab21bfa85d6
CantinaVerse Intern <cantinaverse@gmail.com>
2025-08-19 01:01:17 -0400

    Feat: Update ReputationRegistry.sol Add External Admin registerUser Func

--- src/ReputationRegistry.sol
+++ src/ReputationRegistry.sol
@@ -59,4 +59,23 @@ contract ReputationRegistry is Ownable, ReentrancyGuard {
     error SelfRatingNotAllowed();
 
     constructor(address _owner) Ownable(_owner) {}
+
+    function registerUser(address user) external onlyOwner {
+        if (_reputations[user].isRegistered) {
+            revert UserAlreadyRegistered(user);
+        }
+        
+        _reputations[user] = ReputationData({
+            score: INITIAL_REPUTATION,
+            totalRatings: 0,
+            totalScore: 0,
+            lastUpdateTime: block.timestamp,
+            lastDecayTime: block.timestamp,
+            isRegistered: true
+        });
+        
+        _registeredUsers.push(user);
+        
+        emit UserRegistered(user, INITIAL_REPUTATION);
+    }
 }
