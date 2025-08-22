90aab9ac57761c957f456e03ad7a69025559c281
CantinaVerse Intern <cantinaverse@gmail.com>
2025-08-22 11:52:13 -0400

    Feat: Add _calculateWeightedRating Internal View Func Natspec Comments

--- src/ReputationRegistry.sol
+++ src/ReputationRegistry.sol
@@ -339,6 +339,12 @@ contract ReputationRegistry is Ownable, ReentrancyGuard {
         }
     }
 
+    /**
+     * @dev Calculate weighted rating based on rater's reputation
+     * @param rating The raw rating (0-1000)
+     * @param rater Address of the rater
+     * @return Weighted rating value
+     */
     function _calculateWeightedRating(
         uint256 rating,
         address rater
